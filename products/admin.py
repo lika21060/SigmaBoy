d79c5474d9b7c722b246852958f40457e733f21f
lika21060 <likaasanidze000@gmail.com>
2025-02-01 23:33:43 +0400

    Delete products directory

--- products/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from products.models import Product, Review, ProductTag
-
-admin.site.register(Product)
-admin.site.register(Review)
-admin.site.register(ProductTag)
-
