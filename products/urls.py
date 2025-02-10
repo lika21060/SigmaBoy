98386283c626f67fc05b25188ef4ae6bb78e90d5
lika21060 <likaasanidze000@gmail.com>
2025-02-10 19:35:25 +0400

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from products.views import *
 
 urlpatterns=[
-    path('products/', products_view, name='products'),
-    path('reviews/', review_view, name="reviews"),
+    path('products/', ProductViewSet.as_view(), name='products'),
+    path('products/<int:pk>', ProductViewSet.as_view(), name='product'),
+    path('reviews/', ReviewViewSet.as_view(), name="reviews"),
     path('cart/', cart_view, name='cart'),
     path('tags/<int:product_id>/', product_tag_view, name='product_tags'),
     path('favourites/<int:user_id>/', favorite_product_view, name='favorite_product_view'),
