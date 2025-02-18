8050bc902ce71bfc58e13481f76e5a2700979ec8
lika21060 <likaasanidze000@gmail.com>
2025-02-18 20:43:17 +0400

    Update models.py

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from config.model_utils.models import TimeStampModel
 
-# Create your models here.
+class Category(TimeStampModel):
+    name = models.CharField(max_length=255, unique=True)
+    products = models.ManyToManyField('products.Product', related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+class CategoryImage(TimeStampModel):
+    category = models.ForeignKey('categories.Category', related_name='images', on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='categories/')
+
+    def __str__(self):
+        return f"Image for {self.category.name}"
+
+    class Meta:
+        verbose_name = "Category Image"
+        verbose_name_plural = "Category Images"
