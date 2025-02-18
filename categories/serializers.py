dc9bbcd54eba98b72b1027a5faa86044bf006375
lika21060 <likaasanidze000@gmail.com>
2025-02-18 20:41:01 +0400

    Update serializers.py

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,1 +1,21 @@
+from rest_framework import serializers
+from categories.models import *
+from products.serializers import *
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class CategoryImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CategoryImage
+        fields = ['id', 'image', 'is_active']
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True, source="product_set")  
+    images = CategoryImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'products', 'images']
