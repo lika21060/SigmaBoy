3bd00431c008c4cdb0a38592c022c997979dba8a
lika21060 <likaasanidze000@gmail.com>
2025-02-18 20:56:05 +0400

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import ProductViewSet, ReviewViewSet, ProductTagView, FavoriteProductView, CartView
+from products.views import *
 
 urlpatterns = [
-    path('products/', ProductViewSet.as_view(), name='product-list'), 
-    path('products/<int:pk>/', ProductViewSet.as_view(), name='product-detail'), 
-    path('reviews/', ReviewViewSet.as_view(), name='review-list'),
-    
-    path('product-tags/', ProductTagView.as_view(), name='product-tag-list'),
-    path('favorite-products/', FavoriteProductView.as_view(), name='favorite-product-list'), 
-    path('cart/', CartView.as_view(), name='cart-list'), 
+    path('products/', ProductViewSet.as_view(), name="products"),
+    path('products/<int:pk>', ProductViewSet.as_view(), name='product'),
+    path('products/<int:product_id>/images/', ProductImageViewSet.as_view(), name='image'),
+    path('products/<int:product_id>/images/<int:pk>', ProductImageViewSet.as_view(), name='image'),
+    path('reviews/', ReviewViewSet.as_view(), name="reviews"),
+    path('favorite_products/', FavoriteProductViewSet.as_view(), name='favorite_products'),
+    path('favorite_products/<int:pk>/', FavoriteProductViewSet.as_view(), name='favorite_product'),
+    path('cart/', CartViewSet.as_view(), name='cart'),
+    path('tags/', ProductTagListView.as_view(), name='tags')
 ]
