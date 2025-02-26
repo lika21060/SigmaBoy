eb82df220fb6505758842186954ef0c6659a7913
lika21060 <likaasanidze000@gmail.com>
2025-02-26 22:24:57 +0400

    Update urls.py

--- categories/urls.py
+++ categories/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from categories.views import *
+from rest_framework_nested import routers
+
+router = routers.DefaultRouter()
+router.register('categories', CategoryViewSet)
+
+categories_router = routers.NestedDefaultRouter(router, 'categories', lookup='category')
+categories_router.register('images', CategoryImageViewSet, basename='category-images')
 
 urlpatterns = [
-    path('categories/' ,CategoryListView.as_view(), name='categories'),
-    path('categories/<int:pk>' ,CategoryDetailView.as_view(), name='categories'),
-    path('categories/<int:category_id>/images' ,CategoryImageViewSet.as_view(), name='categories')
-]
+    path('', include(router.urls)),
+    path('', include(categories_router.urls)),
